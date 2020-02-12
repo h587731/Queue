01e5612cc48127615f472523fe3aea22fb8e7857
Windhoes\Eskil <mail.eskil@gmail.com>
2020-02-12 19:54:58 +0100

    engue utvid erfull

--- Koe/src/no/hvl/dat102/tabell/TabellKoe.java
+++ Koe/src/no/hvl/dat102/tabell/TabellKoe.java
@@ -1,6 +1,7 @@
 package no.hvl.dat102.tabell;
 import no.hvl.dat102.adt.KoeADT;
 import no.hvl.dat102.exception.EmptyCollectionException;
+import no.hvl.dat102.kjedet.LinearNode;
 
 
 //********************************************************************
@@ -11,7 +12,7 @@ import no.hvl.dat102.exception.EmptyCollectionException;
 
 public class TabellKoe<T> implements KoeADT<T>{
  private final static int STDK = 100;
- private int bak;
+ private int bak, front, antall; 
  private T[] koe; 
 
  /******************************************************************
@@ -27,11 +28,54 @@ public class TabellKoe<T> implements KoeADT<T>{
  public TabellKoe (int startKapasitet){
     bak = 0;     
     koe = (T[])(new Object[startKapasitet]);
+    antall=0;
   }
 
+ 
+ private boolean erFull() {
+	 
+	 if(bak+1%koe.length==front) {
+		 return true;
+	 }
+	 
+	 return false;
+	 
+ }
+ 
+ 
+ private void utvid() {
+	 T[]nyKoe = (T[])(new Object[koe.length*2+1]);
+	 for(int i = 0; i < koe.length; i++) {
+		 
+		 nyKoe[i] = koe[i];
+		 koe = nyKoe;
+	 }
+	 
+	 
+ }
+ 
 @Override
 public void enQueue(T element) {
-	// TODO Auto-generated method stub
+	
+	if(erFull()) {
+		utvid();
+	}
+	
+	// Er tom?
+	if(koe[front] == null ) {
+		koe[0] = element;
+		front = bak = 0;
+		
+	}
+	 bak = (bak + 1)% koe.length; 
+     koe[bak] = element; 
+     antall++; 
+	
+	// ny element
+	
+	
+	
+	
 	
 }
 
